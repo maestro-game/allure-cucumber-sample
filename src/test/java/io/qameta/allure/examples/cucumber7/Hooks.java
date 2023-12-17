46f7138c409de4ac984ef779cb3f8c6702b71226
Vadim <super.belovvadim@yandex.ru>
2023-12-17 22:01:27 +0300

    Attachment added

--- src/test/java/io/qameta/allure/examples/cucumber7/Hooks.java
+++ src/test/java/io/qameta/allure/examples/cucumber7/Hooks.java
@@ -1,10 +1,12 @@
 package io.qameta.allure.examples.cucumber7;
 
 import io.cucumber.java.AfterStep;
+import io.qameta.allure.Allure;
 
 public class Hooks {
     @AfterStep
     public void waitForSeconds() throws InterruptedException {
+        Allure.attachment("info.txt", "Waiting...");
         Thread.sleep(Long.parseLong(System.getProperty("wait")));
     }
 }
